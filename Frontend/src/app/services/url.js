90ee3cf7477d8db0c73c6542688c2536dd493546
Ashish Patel <99547279+ashish84k@users.noreply.github.com>
2025-09-04 15:41:34 +0530

    Update url.js

--- Frontend/src/app/services/url.js
+++ Frontend/src/app/services/url.js
@@ -1,4 +1,5 @@
 // const url = {localhost:'http://localhost:3000',deployUrl:''};
-const url = {localhost:'https://nodejs-production-ad66.up.railway.app',deployUrl:''};
+// const url = {localhost:'https://nodejs-production-ad66.up.railway.app',deployUrl:''};
+const url = {localhost:'https://task-management84k.onrender.com',deployUrl:''};
 
 export default url;
